b80871a378618d41d9419124e48708012424e29d
michael.cheng <mcheng.work@gmail.com>
2013-11-26 22:06:07 +0800

    Clean up formatting.

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,95 +10,70 @@ class phpdevweb
 		notify  => Service["httpd"]
 	}
 
-	exec { 'yum-update':
-		command => '/usr/bin/yum -y update',
-		require => Exec["grap-epel"]
-	}
-	exec { "grap-epel":
-		command => "/bin/rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm",
-		creates => "/etc/yum.repos.d/epel.repo",
-		alias   => "grab-epel",
+	exec {
+		'yum-update':
+			command => '/usr/bin/yum -y update',
+			require => Exec["grap-epel"]
+			;
+		"grap-epel":
+			command => "/bin/rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm",
+			creates => "/etc/yum.repos.d/epel.repo",
+			alias   => "grab-epel"
+			;
 	}
 	
-	package { "iptables": 
-		ensure => present;
+	package {[
+			"iptables",
+			"vim-enhanced",
+			"git",
+			"httpd",
+			"httpd-devel",
+			"mod_ssl",
+
+		]: 
+			ensure => present;
 	}
 
-	service { "iptables":
-		require => Package["iptables"],
-		hasstatus => true,
-		status => "true",
-		# hasrestart => false,
+	service {
+		"iptables":
+			require    => Package["iptables"],
+			hasstatus  => true,
+			status     => "true",
+			hasrestart => false
+			;
+		"httpd":
+			name      => 'httpd',
+			require   => Package["httpd"],
+			ensure    => running,
+			enable    => true
+			;
 	}
 
-	file { "/etc/sysconfig/iptables":
-		owner   => "root",
-		group   => "root",
-		mode    => 600,
-		replace => true,
-		ensure  => present,
-		source  => "/vagrant/files/iptables.txt",
-		require => Package["iptables"],
-		notify  => Service["iptables"],
-	}
-
-	package { "vim-enhanced":
-		ensure  => present,
-	}
-
-	package { "git":
-		ensure  => present,
-	}
-
-	package { "httpd":
-		ensure => present
-	}
-
-	package { "httpd-devel":
-		ensure  => present
-	}
-
-	package { "mod_ssl":
-		ensure  => present
-	}
-
-	service { 'httpd':
-		name      => 'httpd',
-		require   => Package["httpd"],
-		ensure    => running,
-		enable    => true
-	}
-
-	file { "/etc/httpd/conf.d/vhost.conf":
-		replace => true,
-		ensure  => present,
-		source  => "/vagrant/files/httpd/conf.d/vhost.conf",
-	}
-
-	# Uncomment if you want to create these folders separately
-
-	# file { "/etc/httpd/vhosts":
-	#     ensure => "directory",
-	#   }
-	file { "/etc/httpd/vhosts_ssl":
-		ensure => "directory",
-	}
-	# file { "/etc/httpd/ssl":
-	#     ensure => "directory",
-	#   }
-
-	# How to create a writable folder
-
-	# file { "/var/www/share":
-	#   mode   => 777,
-	#   ensure => "directory",
-	# }
-
-	file { "/etc/httpd/vhosts":
-		replace => true,
-		ensure  => present,
-		source  => "/vagrant/files/httpd/vhosts",
-		recurse => true,
+	file {
+		"/etc/sysconfig/iptables":
+			owner   => "root",
+			group   => "root",
+			mode    => 600,
+			replace => true,
+			ensure  => present,
+			source  => "/vagrant/files/iptables.txt",
+			require => Package["iptables"],
+			notify  => Service["iptables"]
+			;
+		"/etc/httpd/conf.d/vhost.conf":
+			replace => true,
+			ensure  => present,
+			source  => "/vagrant/files/httpd/conf.d/vhost.conf"
+			;
+		"/etc/httpd/vhosts":
+		    ensure => "directory"
+		    ;
+		"/etc/httpd/vhosts":
+			replace => true,
+			ensure  => present,
+			source  => "/vagrant/files/httpd/vhosts",
+			recurse => true
+			;
 	}
 
 	# Uncomment if you want to specify SSL vhosts and SSL folder for your SSL files.
@@ -121,73 +96,28 @@ class phpdevweb
 		config_hash => { 'root_password' => 'media1' }
 	}
 
-	package { "php":
-		ensure  => present,
-	}
-
-	package { "php-cli":
-		ensure  => present,
-	}
-
-	package { "php-common":
-		ensure  => present,
-	}
-
-	package { "php-devel":
-		ensure  => present,
-	}
-
-	package { "php-gd":
-		ensure  => present,
-	}
-
-	package { "php-mcrypt":
-		ensure  => present,
-	}
-
-	package { "php-intl":
-		ensure  => present,
-	}
-
-	package { "php-mbstring":
-		ensure  => present,
-	}
-
-	package { "php-mysql":
-		ensure  => present,
-	}
-
-	package { "php-pdo":
-		ensure  => present,
-	}
-
-	package { "php-pear":
-		ensure  => present,
-	}
-
-	package { "php-soap":
-		ensure  => present,
-	}
-
-	package { "php-xml":
-		ensure  => present,
-	}
-
-	package { "uuid-php":
-		ensure  => present,
-	}
-
-	package { "php-pecl-memcache":
-		ensure  => present,
-	}
-
-	package { "php-pecl-xdebug":
+	package { [
+		"php",
+		"php-cli",
+		"php-common",
+		"php-devel",
+		"php-gd",
+		"php-mcrypt",
+		"php-intl",
+		"php-mbstring",
+		"php-mysql",
+		"php-pdo",
+		"php-pear",
+		"php-soap",
+		"php-xml",
+		"uuid-php",
+		"php-pecl-memcache",
+		"php-pecl-xdebug",
+		"php-pecl-apc",
+		"phpMyAdmin",
+	]:
 		ensure  => present,
 		require => Exec["grab-epel"]
-	}
-
-	package { "php-pecl-apc":
-		ensure  => present,
 	}
 
 	exec { "xhprof":
@@ -196,70 +126,62 @@ class phpdevweb
 		require => Exec["grab-epel"]
 	}
 
-	file { "/etc/php.d/xhprof.ini":
-		replace => true,
-		ensure  => present,
-		source  => "/vagrant/files/php.d/xhprof.ini",
-	}
-
-	file { "/etc/php.ini":
-		replace => true,
-		ensure  => present,
-		source  => "/vagrant/files/php.ini",
-	}
-
-	package { "phpMyAdmin":
-		ensure  => present,
-	}
-
-	file { "/etc/httpd/conf.d/phpMyAdmin.conf":
-		replace => true,
-		ensure  => present,
-		source  => "/vagrant/files/httpd/conf.d/phpMyAdmin.conf",
-	}
-
-	file { "/etc/phpMyAdmin/config.inc.php":
-		replace => true,
-		ensure  => present,
-		source  => "/vagrant/files/phpmy_admin_config.inc.php",
-		require => Package["phpMyAdmin"]
+	file { 
+		"/etc/php.d/xhprof.ini":
+			replace => true,
+			ensure  => present,
+			source  => "/vagrant/files/php.d/xhprof.ini"
+			;
+		"/etc/php.ini":
+			replace => true,
+			ensure  => present,
+			source  => "/vagrant/files/php.ini"
+			;
+		"/etc/httpd/conf.d/phpMyAdmin.conf":
+			replace => true,
+			ensure  => present,
+			source  => "/vagrant/files/httpd/conf.d/phpMyAdmin.conf"
+			;
+		"/etc/phpMyAdmin/config.inc.php":
+			replace => true,
+			ensure  => present,
+			source  => "/vagrant/files/phpmy_admin_config.inc.php",
+			require => Package["phpMyAdmin"]
+			;
 	}
 
 	# PHP TDD Libraries
-
-	# exec { "pear-phpunit-discover":
-	# 	command => "/usr/bin/pear config-set auto_discover 1",
-	# 	require => Package["php-pear"]
+	# exec {
+	# 	"pear-phpunit-discover":
+	# 		command => "/usr/bin/pear config-set auto_discover 1",
+	# 		require => Package["php-pear"]
+	# 		;
+	# 	"pear-phpunit":
+	# 		command => "/usr/bin/pear install phpunit/PHPUnit",
+	# 		creates => "/usr/bin/phpunit",
+	# 		require => Exec["pear-phpunit-discover"]
+	# 		;
+	# 	"pear-phpunit-skeleton-generator":
+	# 		command => "/usr/bin/pear install phpunit/PHPUnit_SkeletonGenerator",
+	# 		creates => "/usr/bin/phpunit-skelgen",
+	# 		require => Exec["pear-phpunit-discover"]
+	# 		;
+	# 	"pear-phpunit-selenium":
+	# 		command => "/usr/bin/pear install phpunit/PHPUnit_Selenium",
+	# 		creates => "/usr/share/pear/PHPUnit/Extensions/Selenium2TestCase.php",
+	# 		require => Exec["pear-phpunit-discover"]
+	# 		;
+	# 	"pear-phpunit-story":
+	# 		command => "/usr/bin/pear install phpunit/PHPUnit_Story",
+	# 		creates => "/usr/share/pear/PHPUnit/Extensions/Story/Step.php",
+	# 		require => Exec["pear-phpunit-discover"]
+	# 		;
+	# 	"pear-phpunit-dbunit":
+	# 		command => "/usr/bin/pear install phpunit/DbUnit",
+	# 		creates => "/usr/share/pear/PHPUnit/Extensions/Database/DefaultTester.php",
+	# 		require => Exec["pear-phpunit-discover"]
+	# 		;
 	# }
 
-	# exec { "pear-phpunit":
-	# 	command => "/usr/bin/pear install phpunit/PHPUnit",
-	# 	creates => "/usr/bin/phpunit",
-	# 	require => Exec["pear-phpunit-discover"]
-	# }
-
-	# exec { "pear-phpunit-skeleton-generator":
-	# 	command => "/usr/bin/pear install phpunit/PHPUnit_SkeletonGenerator",
-	# 	creates => "/usr/bin/phpunit-skelgen",
-	# 	require => Exec["pear-phpunit-discover"]
-	# }
-
-	# exec { "pear-phpunit-selenium":
-	# 	command => "/usr/bin/pear install phpunit/PHPUnit_Selenium",
-	# 	creates => "/usr/share/pear/PHPUnit/Extensions/Selenium2TestCase.php",
-	# 	require => Exec["pear-phpunit-discover"]
-	# }
-
-	# exec { "pear-phpunit-story":
-	# 	command => "/usr/bin/pear install phpunit/PHPUnit_Story",
-	# 	creates => "/usr/share/pear/PHPUnit/Extensions/Story/Step.php",
-	# 	require => Exec["pear-phpunit-discover"]
-	# }
-
-	# exec { "pear-phpunit-dbunit":
-	# 	command => "/usr/bin/pear install phpunit/DbUnit",
-	# 	creates => "/usr/share/pear/PHPUnit/Extensions/Database/DefaultTester.php",
-	# 	require => Exec["pear-phpunit-discover"]
-	# }
 }
 include phpdevweb
